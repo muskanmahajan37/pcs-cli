7ff0efa44cff99db56eae204441e6ea61076fc6c
Hua Zhang <huazhan@microsoft.com>
2017-10-24 16:26:45 +0800

    Add test suite config step before running api tests

--- testsuites/testUtils.js
+++ testsuites/testUtils.js
@@ -13,6 +13,49 @@ TestUtils = {
         })[0];
     },
 
+    loadDeployment: () => {
+        const fs = require('fs');
+        const inquirer = require('inquirer');
+        const configTemplate = require('./testsuites-config-template.json');
+        const configFile = 'testsuites/testsuites-config.json';
+
+        let deployments = [];
+        let files = fs.readdirSync('./deployments');
+
+        files.forEach(f => {
+            deployments.push({ name: f, value: require('../deployments/' + f), short: f } );
+        });
+        deployments.push({ name: 'Will input target service uri...', value: null, short: 'none' });
+
+        let chooseDeploymentQuestion = [{
+            name: 'chooseDeployment',
+            type: 'list',
+            message: 'please choose target deployment to run api test:',
+            choices: deployments,
+        }];
+        let inputServiceUriQuestion = [{
+            name: 'inputServiceUri',
+            type: 'input',
+            message: 'please input service uri:'
+        }];
+        inquirer.prompt(chooseDeploymentQuestion).then(function (answers) {
+            let choosedDeployment = answers.chooseDeployment;
+            if(choosedDeployment){
+                configTemplate.forEach(service => {
+                    service.serviceUrl = service.serviceUrl.replace('{0}', choosedDeployment.website);
+                });
+                fs.writeFileSync(configFile, JSON.stringify(configTemplate, null, 2));
+            } else {
+                inquirer.prompt(inputServiceUriQuestion).then(function(answer){
+                    configTemplate.forEach(service => {
+                        service.serviceUrl = service.serviceUrl.replace('{0}', answer.inputServiceUri);
+                    });
+                    fs.writeFileSync(configFile, JSON.stringify(configTemplate, null, 2));
+                });
+            }
+        });
+    },
+
     says: (message , service) => {
         return message + '(' + service['displayName'] + ')';
     },
