ba76eeca8da38314951babd6fa75c71e01851479
Hua Zhang <huazhan@microsoft.com>
2017-10-24 14:14:36 +0800

    refact testsuites folder and test utils

--- testsuites/pcs-storage-adapter.test.ts
+++ testsuites/pcs-storage-adapter.test.ts
@@ -2,22 +2,23 @@ import * as promiserequest from 'request-promise';
 import { expect, should } from 'chai';
 const TestUtils = require('./testUtils');
 const services: any = TestUtils.loadConfig('pcs-storage-adapter');
+const says = TestUtils.says;
 
-describe('pcs-storage-adapter:StatusController', () => {
+fdescribe('pcs-storage-adapter:StatusController', () => {
     describe('GET    /v1/status ', () => {
         services.forEach((service) => {
-            it('get status of the service (' + service.displayName + ')', () => {
-                const response: any = {};
+            it(says('get status of the service', service), () => {
                 const options: any = {
                     url: service.serviceUrl + '/v1/status',
                     method: 'GET'
                 };
+                const response: any = {};
                 TestUtils.prepareHttpRequest(options, response);
                 return promiserequest(options).then((status) => {
                     expect('StorageAdapter').to.equal(status.Name);
-                    expect('OK:Alive and well').to.equal(status.Status);
+                    expect('OK:Alive and Well!').to.equal(status.Status);
                 }).catch((m) => {
-                    TestUtils.throwTestException(options, response.response, m);
+                    TestUtils.throwTestException(options, response, m);
                 });
             });
         });
