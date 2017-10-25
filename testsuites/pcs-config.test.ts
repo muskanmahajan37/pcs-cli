fa3f9d9dfb161fa221dfe8bc6281061ea0a17e9c
Hua Zhang <huazhan@microsoft.com>
2017-10-25 12:56:06 +0800

    Fix some tests based on current code

--- testsuites/pcs-config.test.ts
+++ testsuites/pcs-config.test.ts
@@ -9,7 +9,7 @@ const services: any = TestUtils.loadConfig('pcs-config');
 
 const dateTimeMatcher = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}/;
 
-xdescribe('Status API', () => {
+describe('Status API', () => {
     describe('GET    /v1/status ', () => {
         services.forEach(service => {
             it(says('should return service status ', service), () => {
@@ -21,7 +21,7 @@ xdescribe('Status API', () => {
                 const response: any = {};
                 TestUtils.prepareHttpRequest(options, response);
                 return rp(options).then((status) => {
-                    expect(status.Name).to.equal('UIConfig');
+                    expect(status.Name).to.equal('Config');
                     expect(status.Status).to.equal('OK:Alive and well');
                     status.should.have.property('CurrentTime').to.match(dateTimeMatcher);
                     status.should.have.property('StartTime').to.match(dateTimeMatcher);
@@ -38,7 +38,7 @@ xdescribe('Status API', () => {
     });
 });
 
-xdescribe('solution-settings API', () => {
+describe('solution-settings API', () => {
 
     describe('GET /v1/solution-settings/theme', () => {
         services.forEach(service => {
@@ -51,8 +51,9 @@ xdescribe('solution-settings API', () => {
                 const response: any = {};
                 TestUtils.prepareHttpRequest(options, response);
                 return rp(options).then((theme) => {
-                    theme.should.have.deep.property('name');
-                    theme.should.have.deep.property('description');
+                    // TODO: API spec is using 'name' and 'description' but code is 'Name', 'Description'
+                    theme.should.have.deep.property('Name');
+                    theme.should.have.deep.property('Description');
                 }).catch(m => {
                     TestUtils.throwTestException(options, response, m);
                 });
@@ -77,8 +78,9 @@ xdescribe('solution-settings API', () => {
                 };
                 const response: any = {};
                 TestUtils.prepareHttpRequest(options, response);
-                return rp(options).then((solutionSettings) => {
-                    expect(response.response).have.deep.property('content-type');
+                return rp(options).then((solutionSettings)=> {
+                    expect(response.response).have.deep.property('headers').to.have.property('content-type', 'image/svg+xml');
+                    expect(solutionSettings).to.match(/.*<svg.*<\/svg>/i);
                 }).catch(m => {
                     TestUtils.throwTestException(options, response, m);
                 });
@@ -88,15 +90,15 @@ xdescribe('solution-settings API', () => {
 
     describe('PUT /v1/solution-settings/logo', () => {
         services.forEach(service => {
-            it(says('Set whole solution settings ', service), () => {
-                // because we do not have delete api, skip this test
+            xit(says('Set whole solution settings ', service), () => {
+                // because we do not have delete api, skip this test to avoid keeping garbage settings.
             });
         });
     });
 
 });
 
-xdescribe('user-settings API', () => {
+describe('user-settings API', () => {
 
     describe('GET /v1/user-settings/{id}', () => {
         services.forEach(service => {
@@ -238,7 +240,7 @@ describe('devicegroups API', () => {
 
     describe('GET /v1/devicegroups', () => {
         services.forEach(service => {
-            it(says('should all device group definitions ', service), () => {
+            it(says('should return all device group definitions ', service), () => {
                 const options: any = {
                     url: service.serviceUrl + '/v1/devicegroups',
                     method: 'GET',
@@ -247,10 +249,10 @@ describe('devicegroups API', () => {
                 const response: any = {};
                 TestUtils.prepareHttpRequest(options, response);
                 return rp(options).then((devicegroups) => {
-                    expect(devicegroups).have.property('items').instanceof(Array);
-                    expect(devicegroups).have.deep.property('$metadata', {
+                    devicegroups.should.have.property('items').to.be.an.instanceof(Array);
+                    devicegroups.should.have.deep.property('$metadata', {
                         '$type': 'DeviceGroupList;1',
-                        '$url': '/v1/devicesgroups'
+                        '$url': '/v1/devicegroups'
                     });
                 }).catch(m => {
                     TestUtils.throwTestException(options, response, m);
@@ -270,13 +272,14 @@ describe('devicegroups API', () => {
                 const response: any = {};
                 TestUtils.prepareHttpRequest(options, response);
                 return rp(options).then((devicegroup) => {
-                    expect(devicegroup).have.property('id', service.devicegroupGet);
-                    expect(devicegroup).have.property('displayName', service.displayName);
-                    expect(devicegroup).have.property('conditions').instanceOf(Array);
-                    expect(devicegroup).have.property('etag').instanceOf(String);
+                    //TODO: API spec is using lowercase names but code is using uppercase names.
+                    expect(devicegroup).have.property('Id', service.devicegroupGet);
+                    expect(devicegroup).have.property('DisplayName', service.displayName);
+                    expect(devicegroup).have.property('Conditions').instanceOf(Array);
+                    expect(devicegroup).have.property('ETag');
                     expect(devicegroup).have.deep.property('$metadata', {
                         '$type': 'DeviceGroup;1',
-                        '$url': '/v1/devicegroups/{id}'
+                        '$url': '/v1/devicegroups/' + service.devicegroupGet
                     });
                 }).catch(m => {
                     TestUtils.throwTestException(options, response, m);
@@ -290,19 +293,19 @@ describe('devicegroups API', () => {
             it(says('Add single device group definition ', service), () => {
                 const options: any = {
                     body: {
-                        'conditions': [
+                        'Conditions': [
                             {
-                                'key': 'key1',
-                                'operator': 'EQ',
-                                'value': 'value1'
+                                'Key': 'key1',
+                                'Operator': 'EQ',
+                                'Value': 'value1'
                             },
                             {
-                                'key': 'key2',
-                                'operator': 'EQ',
-                                'value': 'value2'
+                                'Key': 'key2',
+                                'Operator': 'EQ',
+                                'Value': 'value2'
                             }
                         ],
-                        'displayName': service.displayName + 'add',
+                        'DisplayName': service.displayName + 'add',
                     },
                     json: true,
                     method: 'POST',
@@ -311,7 +314,19 @@ describe('devicegroups API', () => {
                 const response: any = {};
                 TestUtils.prepareHttpRequest(options, response);
                 return rp(options).then((devicegroup) => {
-                    expect(devicegroup).instanceOf(Object);
+                    devicegroup.should.have.deep.property('Conditions', [
+                        {
+                            'Key': 'key1',
+                            'Operator': 'EQ',
+                            'Value': 'value1'
+                        },
+                        {
+                            'Key': 'key2',
+                            'Operator': 'EQ',
+                            'Value': 'value2'
+                        }
+                    ]);
+                    devicegroup.should.have.property('DisplayName', service.displayName + 'add');
                 }).catch(m => {
                     TestUtils.throwTestException(options, response, m);
                 });
@@ -324,20 +339,20 @@ describe('devicegroups API', () => {
             it(says('Update single device group definition ', service), () => {
                 const options: any = {
                     body: {
-                        'conditions': [
+                        'Conditions': [
                             {
-                                'key': 'key1',
-                                'operator': 'LE',
-                                'value': 'value1'
+                                'Key': 'key1',
+                                'Operator': 'LE',
+                                'Value': 'value1'
                             },
                             {
-                                'key': 'key2',
-                                'operator': 'LE',
-                                'value': 'value2'
+                                'Key': 'key2',
+                                'Operator': 'LE',
+                                'Value': 'value2'
                             }
                         ],
-                        'displayName': service.displayName + 'update',
-                        'etag': service.devicegroupUpdate.ETag
+                        'DisplayName': service.displayName + 'update',
+                        'ETag': service.devicegroupUpdate.ETag
                     },
                     json: true,
                     method: 'PUT',
@@ -346,21 +361,21 @@ describe('devicegroups API', () => {
                 const response: any = {};
                 TestUtils.prepareHttpRequest(options, response);
                 return rp(options).then((devicegroup) => {
-                    expect(devicegroup).have.property('id', service.devicegroupUpdate);
-                    expect(devicegroup).have.property('displayName', service.displayName + 'update');
-                    expect(devicegroup).have.property('conditions').to.deep.equals([
+                    expect(devicegroup).have.property('Id', service.devicegroupUpdate.Id);
+                    expect(devicegroup).have.property('DisplayName', service.displayName + 'update');
+                    expect(devicegroup).have.deep.property('Conditions', [
                         {
-                            'key': 'key1',
-                            'operator': 'LE',
-                            'value': 'value1'
+                            'Key': 'key1',
+                            'Operator': 'LE',
+                            'Value': 'value1'
                         },
                         {
-                            'key': 'key2',
-                            'operator': 'LE',
-                            'value': 'value2'
+                            'Key': 'key2',
+                            'Operator': 'LE',
+                            'Value': 'value2'
                         }
                     ]);
-                    expect(devicegroup).have.property('etag').instanceOf(String);
+                    expect(devicegroup).have.property('ETag');
                 }).catch(m => {
                     TestUtils.throwTestException(options, response, m);
                 });
